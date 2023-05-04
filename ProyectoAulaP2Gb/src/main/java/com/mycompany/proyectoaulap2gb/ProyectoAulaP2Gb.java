01e9141347a18fdcb92df4c368c1c5048f8f4e11
octavio4452 <66646120+octavio4452@users.noreply.github.com>
2023-05-04 14:33:30 -0500

    Trabajo Dia 2

---
+++ ProyectoAulaP2Gb/src/main/java/com/mycompany/proyectoaulap2gb/ProyectoAulaP2Gb.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoaulap2gb;
+
+/**
+ *
+ * @author octaviux
+ */
+public class ProyectoAulaP2Gb {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
